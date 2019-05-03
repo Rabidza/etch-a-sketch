6b34e0e3c1842fae8da7afe318739ec01eafafdd
Rabidza <neillhenning@gmail.com>
2019-05-03 12:42:23 +0200

    Add Hover over effect to change colour

--- main.js
+++ main.js
@@ -1,37 +1,22 @@
-// function createGrid(container) {
-    // var grid = document.createElement("div");
-    // grid.id = "grid";
-
-    // for (i=0; i<16; i+=1) {
-        // var row = document.createElement("div");
-        // row.className = "row";
-        // row.id = "row" + i;
-      
-        // for (k=0; k<16; k+=1) {
-            // var box = document.createElement("div"); 
-            // box.className = "box";
-            // row.appendChild(box);
-        // };
-      
-       // return grid.appendChild(row);      
-    // };
-  
-    
-// };
+let gridSize = 16;
 
 function createGrid(){
   let grid = document.createElement("div");
-  grid.id = "grid"
+  grid.id = "grid";
   
-  for (let i = 0; i < 16; i++){
+  for (let i = 0; i < gridSize; i++){
     let row = document.createElement("div");
     row.classname = "row";
     row.id = "row" + i;
     document.getElementById("grid-container").append(row);
     
-    for (let j = 0; j < 16; j++){
+    for (let j = 0; j < gridSize; j++){
       let cell = document.createElement("div");
       cell.className = "cell";
+      cell.id = "cell" + j;
+      cell.addEventListener("mouseenter", function( event ){
+        event.target.style.backgroundColor = "purple";
+      })
       row.appendChild(cell);
     }
   }
